4db5274d9bdb850ad6eee35abe100a69f74ce265
sina-mahdavi-nasab <sinamahdavi109@gmail.com>
2025-11-08 14:33:58 +0100

    Update and rename plot_best_peter.m to plot_best.m

--- plot_best.m
+++ plot_best.m
@@ -1,12 +1,12 @@
 clc
 clear
 
-load('outcmaesxrecentbestPeter.dat')
+load('outcmaesxrecentbest.dat')
 
-[~,bestgen] = min(outcmaesxrecentbestPeter(:,3)) ;
+[~,bestgen] = min(outcmaesxrecentbest(:,3)) ;
 % ABCD_mat = bestABCD;
 % rules = unflattenABCD(ABCD_mat);
-ABCD_mat = outcmaesxrecentbestPeter(bestgen,6:end)';
+ABCD_mat = outcmaesxrecentbest(bestgen,6:end)';
 ABCD_mat = max(ABCD_mat,-5);
 ABCD_mat = min(ABCD_mat,5);
 
@@ -14,14 +14,14 @@ ABCD_mat = min(ABCD_mat,5);
 % load("bestABCD(6)_Collision20_3Wall");
 % ABCD_mat = bestABCD;
 
-rules = unflattenABCDpeter(ABCD_mat);
+rules = unflattenABCD(ABCD_mat);
 seed = 1;
 tic;
 % [plotting, distance_travelled, avg_batt, collsions] = simulation_free_global_mod_2_peter(rules,seed);
-simulation_free_global_mod_2_peter(rules,seed);
+simulation_free_global_mod_2(rules,seed);
 toc;
 
-function R = unflattenABCDpeter(v)
+function R = unflattenABCD(v)
     idx=0; take=@(n) reshape(v(idx+(1:n)),[],1);
     grab=@(r,c) reshape( take(r*c), r,c );
     R.A1 = grab(10,10); idx=idx+100;
@@ -36,4 +36,5 @@ function R = unflattenABCDpeter(v)
     R.D1 = grab(10,10); idx=idx+100;
     R.D2 = grab(10,10); idx=idx+100;
     R.D3 = grab(10, 2);
+
 end
